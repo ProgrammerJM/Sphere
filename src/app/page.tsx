75f712d115d21487c9b6b42de2126ad5c1df820f
John Mark Tizado <johnmarktizado@gmail.com>
2025-01-28 17:15:04 +0800

    add: implement user authentication buttons and create reusable button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello, from home page!</h1>
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
