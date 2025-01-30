2ab56d2bc195e3d32cf742bfd56a91d7dd8ad4b5
John Mark Tizado <johnmarktizado@gmail.com>
2025-01-30 17:40:08 +0800

    refactor: simplify Home component by removing authentication buttons and adding header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,7 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="m-4">
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-
-      <Button variant={"secondary"}>Click me</Button>
+      <h1>Home Page Content</h1>
     </div>
   );
 }
